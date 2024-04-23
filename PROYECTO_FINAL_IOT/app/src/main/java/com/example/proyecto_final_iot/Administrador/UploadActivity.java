fdcac9d911e57eea228e9f22ce2b0ffa005b2e9f
Samantha Sanchez <94878885+SamanthaXimena@users.noreply.github.com>
2024-04-23 04:46:36 -0500

    Vista lista y formulario

--- PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Administrador/UploadActivity.java
+++ PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Administrador/UploadActivity.java
@@ -1,7 +1,9 @@
-package com.example.proyecto_final_iot;
+package com.example.proyecto_final_iot.Administrador;
+
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -10,38 +12,49 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.proyecto_final_iot.Administrador.Admin_lista;
-import com.google.firebase.database.core.view.View;
+import com.example.proyecto_final_iot.MainActivity;
+import com.example.proyecto_final_iot.R;
 
-public class MainActivity extends AppCompatActivity {
 
-    Button boton_Registrar;
+public class UploadActivity extends AppCompatActivity {
+
+    Button BotonAtras;
+    Button BotonGuardar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_upload);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        boton_Registrar = findViewById(R.id.login_button);
-        boton_Registrar.setOnClickListener(new View.OnClickListener() {
+
+        BotonAtras = findViewById(R.id.backButton);
+        BotonAtras.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(MainActivity.this, Admin_lista.class);
+                Intent intent = new Intent(UploadActivity.this, Admin_lista.class);
                 startActivity(intent);
             }
         });
-        setContentView(R.layout.bar_menu);
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-        //    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-         //   return insets;
-        //});
-    }
-}
 
+        BotonGuardar = findViewById(R.id.saveButton);
+        BotonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(UploadActivity.this, Admin_lista.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+
+}
