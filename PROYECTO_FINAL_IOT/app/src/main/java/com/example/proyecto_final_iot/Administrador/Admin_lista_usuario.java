f81465b2b001ca3c65ce1e5dd6d0d82d9fc433c1
Samantha Sanchez <94878885+SamanthaXimena@users.noreply.github.com>
2024-04-28 17:26:09 -0500

    arreglo pequeñitos errores

--- PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Administrador/Admin_lista_usuario.java
+++ PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Administrador/Admin_lista_usuario.java
@@ -16,7 +16,7 @@ public class Admin_lista_usuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_lista_usuarios);
+       // setContentView(R.layout.activity_admin_lista_usuarios);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
