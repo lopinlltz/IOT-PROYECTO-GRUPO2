0b73ec372d647f13e20b484d3d6fc50071475a0c
joselin <a20206442@pucp.edu.pe>
2024-04-28 13:19:07 -0500

    formularios Supervisor

---
+++ PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Supervisor/Activity/NuevoReporteActivity.java
@@ -0,0 +1,69 @@
+package com.example.proyecto_final_iot.Supervisor.Activity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.proyecto_final_iot.R;
+
+public class NuevoReporteActivity extends AppCompatActivity {
+
+    private LinearLayout atras;
+    private LinearLayout Guardar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.supervisor_nuevo_equipo);
+
+        atras =  findViewById(R.id.atras);
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(NuevoReporteActivity.this,OpcionesActivity.class));
+            }
+        });
+
+        Guardar =  findViewById(R.id.Guardar);
+        Guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(NuevoReporteActivity.this,OpcionesActivity.class));
+                ConfirmacionPopup();
+            }
+        });
+
+    }
+
+    private void ConfirmacionPopup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("¿Estas seguro de enviar el reporte?");
+
+
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(NuevoReporteActivity.this, HistorialSupervisorActivity.class);
+                ConfirmacionPopup();
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+}
--- PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Supervisor/Adapter/EquipoSupervisorAdapter.java
+++ PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Supervisor/Adapter/EquipoSupervisorAdapter.java
@@ -1,4 +1,4 @@
-package com.example.proyecto_final_iot;
+package com.example.proyecto_final_iot.Supervisor.Adapter;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +7,9 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.proyecto_final_iot.EquipoData;
+import com.example.proyecto_final_iot.R;
 
 import java.util.List;
 
