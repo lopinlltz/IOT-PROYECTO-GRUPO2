559162dc33e4d6a54b9cfa73e92b5ac984cfb2bb
= <=>
2024-04-29 01:58:16 -0500

    vista superamin principal con recycler view OK.

--- PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Superadmin/Activity/Superadmin_vista_principal1.java
+++ PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Superadmin/Activity/Superadmin_vista_principal1.java
@@ -9,18 +9,44 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.proyecto_final_iot.R;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.proyecto_final_iot.R;
+import com.example.proyecto_final_iot.SitioData;
+import com.example.proyecto_final_iot.Superadmin.Adapter.AdminAdapter;
+import com.example.proyecto_final_iot.Superadmin.Data.Admin;
+import com.example.proyecto_final_iot.Supervisor.Adapter.SitioSupervisorAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Superadmin_vista_principal1 extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private AdminAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_superadmin_vista_principal1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        recyclerView = findViewById(R.id.recycler_view_admin);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Admin> dataList = new ArrayList<>();
+        dataList.add(new Admin("13:10", "Maricielo"));
+        dataList.add(new Admin("13:10", "Joselin"));
+        dataList.add(new Admin("13:10", "Samantha"));
+        dataList.add(new Admin("13:10", "Massiel"));
+        dataList.add(new Admin("13:10", "Ricardo"));
+        dataList.add(new Admin("13:10", "Diego"));
+        dataList.add(new Admin("13:10", "Pablo"));
+        dataList.add(new Admin("13:10", "Nana"));
+
+        adapter = new AdminAdapter(dataList);
+        recyclerView.setAdapter(adapter);
     }
 }
