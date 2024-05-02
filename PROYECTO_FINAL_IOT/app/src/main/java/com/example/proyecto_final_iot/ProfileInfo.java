865156258c6e892794045641a5ce29f85cbbaa03
= <=>
2024-05-01 19:50:16 -0500

    cs

--- PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/ProfileInfo.java
+++ PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/ProfileInfo.java
@@ -12,8 +12,8 @@ public class ProfileInfo extends AppCompatActivity {
         setContentView(R.layout.user_profile);
         //fragmento de barra de menu uu
         MenuBarFragment menuBarFragment = new MenuBarFragment();
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragmentContainerMenu, menuBarFragment)
-                .commit();
+        //getSupportFragmentManager().beginTransaction()
+                //.add(R.id.fragmentContainerMenu, menuBarFragment)
+                //.commit();
     }
 }
