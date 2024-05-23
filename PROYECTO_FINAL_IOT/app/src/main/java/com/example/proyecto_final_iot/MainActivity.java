f5d509ada5e5b9cbc925a6cea28b3c5d56683b85
Massiel <a20193470@pucp.edu.pe>
2024-05-23 12:13:45 -0500

    prueba de notificaciones a disp

--- PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/MainActivity.java
+++ PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.proyecto_final_iot;
 
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,12 +28,16 @@ import com.example.proyecto_final_iot.Supervisor.Activity.SitioSupervisorActivit
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //private static final String POST_NOTIFICATIONS = "android.permission.POST_NOTIFICATIONS";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        createNotificationChannel();
+        askPermission();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -67,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 NotificationHelper.createNotificationChannel(MainActivity.this);
                 NotificationHelper.sendNotification(MainActivity.this, "Inicio de sesión", "Bienvenido, superadmin");
+                lanzarNotificacion();
             }
         });
 
@@ -78,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 NotificationHelper.createNotificationChannel(MainActivity.this);
                 NotificationHelper.sendNotification(MainActivity.this, "Inicio de sesión", "Bienvenido, admin");
+                lanzarNotificacion();
             }
         });
 
@@ -89,8 +105,51 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 NotificationHelper.createNotificationChannel(MainActivity.this);
                 NotificationHelper.sendNotification(MainActivity.this, "Inicio de sesión", "Bienvenido, supervisor");
+                lanzarNotificacion();
             }
         });
+    }
+
+    private void createNotificationChannel() {
+        String channelId = "channelDefaultPri";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Canal notificaciones default",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Canal para notificaciones con prioridad default");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void askPermission() {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+                ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
+                        PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{POST_NOTIFICATIONS},
+                    101);
+        }
+    }
+
+    public void lanzarNotificacion() {
+        String channelId = "channelDefaultPri";
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.notifpic)
+                .setContentTitle("Notificación PRUEBA")
+                .setContentText("Funciona ?")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        if (ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(1, builder.build());
+        }
     }
 }
 
