463e2482f24b60f2ed73871e6b30bf699302b5fe
= <=>
2024-06-06 13:45:29 -0500

    search sup dorregio

--- PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Superadmin/Activity/Superadmin_vista_principal1.java
+++ PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Superadmin/Activity/Superadmin_vista_principal1.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.SearchView;
+import androidx.appcompat.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
