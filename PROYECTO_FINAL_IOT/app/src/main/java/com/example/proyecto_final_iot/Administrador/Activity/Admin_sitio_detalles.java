6361cdc88d244c3cb5bcfbb724424823acde2631
Samantha Sanchez <94878885+SamanthaXimena@users.noreply.github.com>
2024-06-06 13:27:21 -0500

    avance: editar pero no actualiza x.x

--- PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Administrador/Activity/Admin_sitio_detalles.java
+++ PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Administrador/Activity/Admin_sitio_detalles.java
@@ -1,5 +1,6 @@
 package com.example.proyecto_final_iot.Administrador.Activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,12 +11,16 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.proyecto_final_iot.Administrador.Data.Sitio_Data;
+import com.example.proyecto_final_iot.NotificationHelper;
+import com.example.proyecto_final_iot.Supervisor.Activity.EquipoDetalleActivity;
+import com.example.proyecto_final_iot.Supervisor.Activity.EquiposSupervisorActivity;
 import com.example.proyecto_final_iot.databinding.ActivityAdminSitioDetallesBinding;
 import com.github.clans.fab.FloatingActionButton;
 import com.example.proyecto_final_iot.R;
@@ -28,6 +33,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -39,89 +45,138 @@ public class Admin_sitio_detalles extends AppCompatActivity {
 
     FloatingActionButton boton_delete;
     FloatingActionButton boton_edit;
-    TextView id_codigodeSitio, id_departamento, id_provincia, id_distrito, id_ubigeo,
-            id_tipo_de_zona, id_tipo_de_sitio, id_latitud_long;
-    String id_Documento;
+    TextView id_codigodeSitio_tw, id_departamento_tw, id_provincia_tw, id_distrito_tw, id_ubigeo_tw,
+            id_tipo_de_zona_tw, id_tipo_de_sitio_tw, id_latitud_long_tw;
+
 
 
     private DatabaseReference databaseReference;
-
+    FirebaseFirestore db ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_sitio_detalles);
+        db = FirebaseFirestore.getInstance();
 
-        id_codigodeSitio = findViewById(R.id.id_codigodeSitio);
-        id_departamento = findViewById(R.id.id_departamento);
-        id_provincia = findViewById(R.id.id_provincia);
-        id_distrito = findViewById(R.id.id_distrito);
-        id_ubigeo = findViewById(R.id.id_ubigeo);
-        id_tipo_de_zona = findViewById(R.id.id_tipoDeZona_det);
-        id_tipo_de_sitio = findViewById(R.id.id_tipoDeSitio_det);
-        id_latitud_long = findViewById(R.id.id_latitud_long);
-
-
-
-        Bundle bundle = getIntent().getExtras();
-        if(bundle != null){
-            id_codigodeSitio.setText(bundle.getString("id_codigodeSitio"));
-            id_departamento.setText(bundle.getString("id_departamento"));
-            id_provincia.setText(bundle.getString("id_provincia"));
-            id_distrito.setText(bundle.getString("id_distrito"));
-            id_ubigeo.setText(bundle.getString("id_ubigeo"));
-            id_tipo_de_zona.setText(bundle.getString("id_tipo_de_zona"));
-            id_tipo_de_sitio.setText(bundle.getString("id_tipo_de_sitio"));
-            id_latitud_long.setText(bundle.getString("id_latitud_long"));
-            id_Documento= bundle.getString("documentoID");
-        }
+        Intent intent = getIntent();
+        String id_codigodeSitio = intent.getStringExtra("id_codigodeSitio");
+        String id_departamento = intent.getStringExtra("id_departamento");
+        String id_provincia = intent.getStringExtra("id_provincia");
+        String id_distrito =  intent.getStringExtra("id_distrito");
+        String id_ubigeo =  intent.getStringExtra("id_ubigeo");
+        String id_tipo_de_zona =  intent.getStringExtra("id_tipo_de_zona");
+        String id_tipo_de_sitio =  intent.getStringExtra("id_tipo_de_sitio");
+        String id_latitud_long =  intent.getStringExtra("id_latitud_long");
 
 
 
-        boton_delete = (FloatingActionButton) findViewById(R.id.boton_delete);
+        id_codigodeSitio_tw= findViewById(R.id.id_codigodeSitio);
+        id_departamento_tw = findViewById(R.id.id_departamento);
+        id_provincia_tw = findViewById(R.id.id_provincia);
+        id_distrito_tw = findViewById(R.id.id_distrito);
+        id_ubigeo_tw = findViewById(R.id.id_ubigeo);
+        id_tipo_de_zona_tw = findViewById(R.id.id_tipoDeZona_det);
+        id_tipo_de_sitio_tw = findViewById(R.id.id_tipoDeSitio_det);
+        id_latitud_long_tw = findViewById(R.id.id_latitud_long);
+
+
+        id_codigodeSitio_tw.setText(id_codigodeSitio);
+        id_departamento_tw.setText(id_departamento);
+        id_provincia_tw.setText(id_provincia);
+        id_distrito_tw.setText(id_distrito);
+        id_ubigeo_tw.setText(id_ubigeo);
+        id_tipo_de_zona_tw.setText(id_tipo_de_zona);
+        id_tipo_de_sitio_tw.setText(id_tipo_de_sitio);
+        id_latitud_long_tw.setText(id_latitud_long);
+
+
+
+        boton_delete = findViewById(R.id.boton_delete);
         boton_edit = findViewById(R.id.boton_edit);
 
         boton_delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
-                String documentId = "test"; // Reemplaza con el ID del documento que deseas eliminar
-                db.collection("sitio").document(documentId).delete()
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                // Manejar el éxito de la eliminación
-
-                                Toast.makeText(Admin_sitio_detalles.this, "Deleted", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(getApplicationContext(), Admin_lista_Sitio.class));
-                                finish();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                // Manejar el fallo en la eliminación
-                                Toast.makeText(Admin_sitio_detalles.this, "Error al eliminar el documento", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            }
+                Intent intent = new Intent(Admin_sitio_detalles.this, Admin_lista_Sitio.class);
+                ConfirmacionPopup(id_codigodeSitio);
+                }
         });
-
-
 
         boton_edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Admin_sitio_detalles.this, Admin_sitio_editar.class);
+                intent.putExtra("id_codigodeSitio", id_codigodeSitio);
+                intent.putExtra("id_departamento", id_departamento);
+                intent.putExtra("id_provincia", id_provincia);
+                intent.putExtra("id_distrito", id_distrito);
+                intent.putExtra("id_ubigeo", id_ubigeo);
+                intent.putExtra("id_tipo_de_zona", id_tipo_de_zona);
+                intent.putExtra("id_tipo_de_sitio", id_tipo_de_sitio);
+                intent.putExtra("id_latitud_long", id_latitud_long);
                 startActivity(intent);
+
+            }
+        });
+    }
+
+    private void ConfirmacionPopup(String sitioID) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("¿Estas seguro de eliminar el sitio?");
+
+
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Admin_sitio_detalles.this, Admin_lista_Sitio.class);
+                borrarSitioporCodigo(sitioID);
+                startActivity(intent);
+                dialog.dismiss();
+
+                NotificationHelper.createNotificationChannel(Admin_sitio_detalles.this);
+                NotificationHelper.sendNotification(Admin_sitio_detalles.this, "Sitio", "sitio borrado");
             }
         });
 
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
+    private void borrarSitioporCodigo(String id_codigodeSitio) {
+
+        db.collection("sitio")
+                .whereEqualTo("id_codigodeSitio", id_codigodeSitio)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+
+                            String sitio_ID = document.getId();
+                            db.collection("sitio").document(sitio_ID)
+                                    .delete()
+                                    .addOnSuccessListener(unused -> {
+                                        Toast.makeText(this, "Sitio con Codigo " + id_codigodeSitio + " eliminado correctamente", Toast.LENGTH_SHORT).show();
+                                    })
+                                    .addOnFailureListener(e -> {
+                                        // Error
+                                        Toast.makeText(this, "No se pudo eliminar el Sitio con Codigo " + id_codigodeSitio, Toast.LENGTH_SHORT).show();
+                                    });
+                        }
+                    } /*else {
+                        // Error al realizar la consulta
+                        Toast.makeText(this, "Error al buscar el equipo con SKU " + sku, Toast.LENGTH_SHORT).show();
+                    }*/
+                });
+    }
 
 
 }
--- PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Administrador/Activity/Admin_sitio_editar.java
+++ PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Administrador/Activity/Admin_sitio_editar.java
@@ -1,12 +1,15 @@
 package com.example.proyecto_final_iot.Administrador.Activity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -17,37 +20,177 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.proyecto_final_iot.NotificationHelper;
 import com.example.proyecto_final_iot.R;
+import com.example.proyecto_final_iot.Supervisor.Activity.EquipoDetalleActivity;
+import com.example.proyecto_final_iot.Supervisor.Activity.EquipoEditarActivity;
+import com.example.proyecto_final_iot.Supervisor.Activity.EquiposSupervisorActivity;
+import com.github.clans.fab.FloatingActionButton;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Admin_sitio_editar extends AppCompatActivity {
 
-    EditText id_codigodeSitio,id_departamento,id_provincia,id_distrito,id_ubigeo,id_tipo_de_zona,id_tipo_de_sitio,id_latitud_long;
+    EditText id_departamento_et,id_provincia_et,id_distrito_et,id_ubigeo_et,id_tipo_de_zona_et,id_tipo_de_sitio_et,id_latitud_long_et;
+    TextView id_codigodeSitio_tv;
 
     private String selectTipoDeZona;
     private ArrayAdapter<CharSequence> zonaAdapter;
     private String selectTipoDeSitio;
-
+    FirebaseFirestore db ;
+    FloatingActionButton boton_guardar_sitio;
+    FloatingActionButton boton_atras_sitio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_sitio_editar);
+        db = FirebaseFirestore.getInstance();
 
-        id_codigodeSitio = findViewById(R.id.id_codigodeSitio);
-        id_departamento = findViewById(R.id.id_departamento);
-        id_provincia = findViewById(R.id.id_provincia);
-        id_distrito = findViewById(R.id.id_distrito);
-        id_ubigeo = findViewById(R.id.id_ubigeo);
-        id_tipo_de_zona = findViewById(R.id.id_tipoDeZona);
-        id_tipo_de_sitio = findViewById(R.id.id_tipoDeSitio);
-        id_latitud_long = findViewById(R.id.id_latitud_long);
+        Intent intent = getIntent();
+        String id_codigodeSitio = intent.getStringExtra("id_codigodeSitio");
+        String id_departamento = intent.getStringExtra("id_departamento");
+        String id_provincia = intent.getStringExtra("id_provincia");
+        String id_distrito =  intent.getStringExtra("id_distrito");
+        String id_ubigeo =  intent.getStringExtra("id_ubigeo");
+        String id_tipo_de_zona =  intent.getStringExtra("id_tipo_de_zona");
+        String id_tipo_de_sitio =  intent.getStringExtra("id_tipo_de_sitio");
+        String id_latitud_long =  intent.getStringExtra("id_latitud_long");
 
+        id_codigodeSitio_tv = findViewById(R.id.id_codigodeSitio);
+        id_departamento_et = findViewById(R.id.id_departamento);
+        id_provincia_et = findViewById(R.id.id_provincia);
+        id_distrito_et = findViewById(R.id.id_distrito);
+        id_ubigeo_et = findViewById(R.id.id_ubigeo);
+        id_tipo_de_zona_et = findViewById(R.id.id_tipoDeZona);
+        id_tipo_de_sitio_et = findViewById(R.id.id_tipoDeSitio);
+        id_latitud_long_et = findViewById(R.id.id_latitud_long);
 
+        id_codigodeSitio_tv.setText(id_codigodeSitio);
+        id_departamento_et.setText(id_departamento);
+        id_provincia_et.setText(id_provincia);
+        id_distrito_et.setText(id_distrito);
+        id_ubigeo_et.setText(id_ubigeo);
+        id_tipo_de_zona_et.setText(id_tipo_de_zona);
+        id_tipo_de_sitio_et.setText(id_tipo_de_sitio);
+        id_latitud_long_et.setText(id_latitud_long);
 
+        boton_atras_sitio = findViewById(R.id.boton_back_sitio);
+        boton_atras_sitio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Admin_sitio_editar.this, Admin_lista_Sitio.class);
+                intent.putExtra("id_codigodeSitio", id_codigodeSitio);
+                intent.putExtra("id_departamento", id_departamento );
+                intent.putExtra("id_provincia", id_provincia);
+                intent.putExtra("id_distrito", id_distrito);
+                intent.putExtra("id_ubigeo",id_ubigeo);
+                intent.putExtra("id_tipo_de_zona", id_tipo_de_zona);
+                intent.putExtra("id_tipo_de_sitio", id_tipo_de_sitio);
+                intent.putExtra("id_latitud_long", id_latitud_long);
+                v.getContext().startActivity(intent);
+                startActivity(intent);
+            }
+        });
+        boton_guardar_sitio = findViewById(R.id.boton_save_sitio);
+        boton_guardar_sitio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nuevaDepartamento = id_departamento_et.getText().toString();
+                String nuevoProvincia = id_provincia_et.getText().toString();
+                String nuevaDistrito = id_distrito_et.getText().toString();
+                String nuevaUbigeo = id_ubigeo_et.getText().toString();
+                String nuevaZona = id_tipo_de_zona_et.getText().toString();
+                String nuevaSitio = id_tipo_de_sitio_et.getText().toString();
+                String nuevaLong = id_latitud_long_et.getText().toString();
+
+                Log.d("Admin_sitio_editar", "Datos obtenidos para edición: " +
+                        "id_codigodeSitio=" + id_codigodeSitio +
+                        "nuevaDepartamento=" + nuevaDepartamento +
+                        ", nuevoProvincia=" + nuevoProvincia +
+                        ", nuevaDistrito=" + nuevaDistrito +
+                        ", nuevaUbigeo=" + nuevaUbigeo+
+                        ", nuevaZona=" + nuevaZona+
+                        ", nuevaSitio=" + nuevaSitio+
+                        ", nuevaLong=" + nuevaLong);
+
+                ConfirmacionPopup(id_codigodeSitio, nuevaDepartamento, nuevoProvincia, nuevaDistrito, nuevaUbigeo,nuevaZona,nuevaSitio, nuevaLong);
+            }
+        });
     }
 
+    private void ConfirmacionPopup(String id_codigodeSitio ,String nuevaDepartamento, String nuevoProvincia, String nuevaDistrito, String nuevaUbigeo,  String nuevaZona, String nuevaSitio, String nuevaLong) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("¿Estas seguro de guardar los cambios?");
+
+
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                editarSitio(id_codigodeSitio, nuevaDepartamento, nuevoProvincia, nuevaDistrito, nuevaUbigeo, nuevaZona,  nuevaSitio, nuevaLong);
+                Intent intent = new Intent(Admin_sitio_editar.this, Admin_lista_Sitio.class);
+                startActivity(intent);
+                dialog.dismiss();
+
+                NotificationHelper.createNotificationChannel(Admin_sitio_editar.this);
+                NotificationHelper.sendNotification(Admin_sitio_editar.this, "Sitio", "sitio editado");
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void editarSitio(String id_codigodeSitio, String nuevaDepartamento, String nuevoProvincia, String nuevaDistrito , String nuevaUbigeo, String nuevaZona, String nuevaSitio, String nuevaLong ) {
+        db.collection("sitio")
+                .whereEqualTo("id_codigodeSitio", id_codigodeSitio)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        if (!task.getResult().isEmpty()) {
+
+                            DocumentReference sitioRef = task.getResult().getDocuments().get(0).getReference();
+
+                            // Actualizar el documento
+                            sitioRef.update("id_departamento", nuevaDepartamento,
+                                            "id_provincia", nuevoProvincia,
+                                            "id_distrito", nuevaDistrito,
+                                            "id_ubigeo", nuevaUbigeo,
+                                            "id_tipo_de_zona", nuevaZona,
+                                            "id_tipo_de_sitio", nuevaSitio,
+                                            "id_latitud_long", nuevaLong)
+                                    .addOnSuccessListener(aVoid -> {
+                                        Log.d("Admin_sitio_editar", "Sitio actualizado con éxito");
+                                        finish();
+                                    })
+                                    .addOnFailureListener(e -> {
+                                        Log.e("Admin_sitio_editar", "Error al actualizar el sitio", e);
+
+                                    });
+                        } else {
+
+                            Log.e("Admin_sitio_editar", "El documento con Codigo " + id_codigodeSitio + " no existe.");
+                        }
+                    } else {
+                        Log.e("Admin_sitio_editar ", "Error al obtener el documento", task.getException());
+                    }
+                });
+    }
+
+
 }
+
