ca8189df0742472e30d25a47eb11229df62ee6ae
Maricielo Romo Reyes <a20200825@pucp.edu.pe>
2024-06-20 13:17:29 -0500

    buscador de historial superadmin

--- PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Superadmin/Activity/superadmin_logs.java
+++ PROYECTO_FINAL_IOT/app/src/main/java/com/example/proyecto_final_iot/Superadmin/Activity/superadmin_logs.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import androidx.activity.EdgeToEdge;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.proyecto_final_iot.R;
 import com.example.proyecto_final_iot.Superadmin.Adapter.HistorialAdapter;
 import com.example.proyecto_final_iot.Superadmin.Data.HistorialData;
@@ -16,6 +19,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,16 +27,21 @@ public class superadmin_logs extends AppCompatActivity {
     private RecyclerView recyclerView;
     private HistorialAdapter adapter;
     private FirebaseFirestore db;
+    private List<HistorialData> historialList;
+    private List<HistorialData> filteredHistorialList;
+    private SearchView searchLogs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_superadmin_logs);
+
         ImageButton btnUserProfile = findViewById(R.id.imageButton6);
         ImageButton btnHome = findViewById(R.id.buttonhomesuper);
         ImageButton btnHistory = findViewById(R.id.buttonhistorialsuper);
         ImageButton buttonsupervisor = findViewById(R.id.buttonsupervisor);
+        searchLogs = findViewById(R.id.searchlogs);
+
         buttonsupervisor.setOnClickListener(v -> {
             Intent intent = new Intent(superadmin_logs.this, superadmin_vista_supervisor2.class);
             startActivity(intent);
@@ -57,11 +66,11 @@ public class superadmin_logs extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         db = FirebaseFirestore.getInstance();
-
-        List<HistorialData> historialList = new ArrayList<>();
+        historialList = new ArrayList<>();
+        filteredHistorialList = new ArrayList<>();
 
         db.collection("historialglobal")
-                .orderBy("date", Query.Direction.DESCENDING)  // Ordenar por fecha en orden descendente
+                .orderBy("date", Query.Direction.DESCENDING)
                 .get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
@@ -70,10 +79,42 @@ public class superadmin_logs extends AppCompatActivity {
                             HistorialData historialData = documentSnapshot.toObject(HistorialData.class);
                             historialList.add(historialData);
                         }
-
-                        adapter = new HistorialAdapter(historialList);
+                        filteredHistorialList.addAll(historialList);
+                        adapter = new HistorialAdapter(filteredHistorialList);
                         recyclerView.setAdapter(adapter);
                     }
                 });
+
+        searchLogs.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterList(newText);
+                return true;
+            }
+        });
+    }
+
+    private void filterList(String text) {
+        filteredHistorialList.clear();
+        if (text.isEmpty()) {
+            filteredHistorialList.addAll(historialList);
+        } else {
+            text = text.toLowerCase();
+            for (HistorialData item : historialList) {
+                if (item.getActivityName().toLowerCase().contains(text) ||
+                        item.getUserName().toLowerCase().contains(text) ||
+                        item.getUserRole().toLowerCase().contains(text) ||
+                        item.getDate().toLowerCase().contains(text) ||
+                        item.getHour().toLowerCase().contains(text)) {
+                    filteredHistorialList.add(item);
+                }
+            }
+        }
+        adapter.notifyDataSetChanged();
     }
 }
